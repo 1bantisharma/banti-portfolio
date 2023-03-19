1c5fffb698cb8d9128bf353830154960451882b0
VISHWAJEET KUMAR <98245732+1bantisharma@users.noreply.github.com>
2023-03-19 13:46:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new typed(".multiple-text", {
+    String: ["Web Development","Programming","Problem solving"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
