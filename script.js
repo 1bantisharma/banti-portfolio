6e53c469e717d265e794e09875dcfa3cadd47886
VISHWAJEET KUMAR <98245732+1bantisharma@users.noreply.github.com>
2023-04-29 09:42:38 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var typed=new typed(".multiple-text", {
-    String: ["Web Development","Programming","Problem solving"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
